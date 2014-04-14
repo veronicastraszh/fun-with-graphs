78e7740e3299322bd0df650c0c9d997f39557fed
Veronica Straszheim <veronica.straszh@gmail.com>
2014-04-14 19:24:16 -0400

    fixed busted iterator

--- graph.h
+++ graph.h
@@ -18,8 +18,7 @@ namespace graph {
   
   // A graph
   
-  template<class E>
-    class Graph {
+  template<class E> class Graph {
   public:
     using edge_type = E;
     
@@ -55,57 +54,27 @@ namespace graph {
   
   template<class E> class Graph_Iterator {
   public:
-    Graph_Iterator(const Graph<E>* g, int n) : graph{g}, at_end{false} { enter_node(n); }
-    
-    const E& operator*() { return *current; }
+    Graph_Iterator(const Graph<E>* g, int n) : graph{g}, node{n}, offset{0} {};
+  
+    const E& operator*() { return (*graph)[node][offset]; }
     Graph_Iterator operator++(int) { auto old = *this; advance(); return old; }
     Graph_Iterator& operator++() { advance(); return *this; }
-    void advance();
     
-    bool operator==(const Graph_Iterator<E>& o);
-    bool operator!=(const Graph_Iterator<E>& o);
+    bool operator==(const Graph_Iterator<E>& o) { return graph == o.graph && node == o.node && offset == o.offset; }
+    bool operator!=(const Graph_Iterator<E>& o) { return !(*this).operator==(o); }
   private:
     const Graph<E>* graph;
-    int cur_node;
-    bool at_end;
-    typename vector<E>::const_iterator current;
-    typename vector<E>::const_iterator end;
-    
-    void enter_node(int node);
+    int node;
+    int offset;
+
+    void advance() {
+      offset++;
+      if(offset == (*graph)[node].size()) {
+	offset = 0;
+	node++;
+      }
+    }
   };
-  
-  template<class E> bool Graph_Iterator<E>::operator==(const Graph_Iterator<E>& o) {
-    return graph == o.graph
-    && cur_node == o.cur_node
-    && at_end == o.at_end
-    && current == o.current;
-  }
-  
-  template<class E>
-    bool Graph_Iterator<E>::operator!=(const Graph_Iterator<E>& o) {
-    return !(*this).operator==(o);
-  }
-  
-  template<class E> void Graph_Iterator<E>::advance() {
-    cout << "enter advance, cur_node=" << cur_node << ' ' << reinterpret_cast<long long>(&*current) << '\n';
-    current++;
-    while (current == end && !at_end) {
-      enter_node(cur_node+1);
-    }
-  }
-  
-  template<class E> void Graph_Iterator<E>::enter_node(int node) {
-    cout << "enter enter_node, node=" << node << '\n';
-    if (node < graph->node_count()) {
-      const vector<E> edges = (*graph)[node];
-      current = edges.begin();
-      end = edges.end();
-    } else {
-      current = end = typename vector<E>::const_iterator{};
-      at_end = true;
-    }
-    cur_node = node;
-  }
   
 }
       
--- test.cpp
+++ test.cpp
@@ -5,31 +5,20 @@
 using namespace std;
 using namespace graph;
 
-Graph<Weighted_Edge> duale(const Graph<Weighted_Edge>& g) {
-  Graph<Weighted_Edge> result{};
-  auto end = g.end();
-  for (auto current = g.begin(); current != end; current++) {
-    cout << *current << '\n';
-    result += Weighted_Edge{0,0,1}; //reverse_edge(e);
-  }
-  return result;
-}
-
 int main() {
-  // Graph<Edge> graph = random_graph(5,15);
-  // for (Edge e: graph) {
-  //   cout << e << '\n';
-  // }
+  Graph<Edge> graph = random_graph(5,15);
+  for (Edge e: graph) {
+    cout << e << '\n';
+  }
 
   cout << "Weighted\n";
   Graph<Weighted_Edge> w_graph = random_weighted_graph(5,15,8);
   for (Weighted_Edge e : w_graph) {
     cout << e << '\n';
   }
-  // cout << "Reversed\n";
-  // for (Weighted_Edge e : dual(w_graph)) {
-  //   cout << e << '\n';
-  // }
-  cout << "Fred\n";
-  duale(w_graph);
+
+  cout << "Reversed\n";
+  for (Weighted_Edge e : dual(w_graph)) {
+    cout << e << '\n';
+  }
 }
